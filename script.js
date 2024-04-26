cd95dd302bb1f29c74858cf8d5ebb04a8ae8995d
Lululak <168170391+Lululak@users.noreply.github.com>
2024-04-26 17:28:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert('Welcome to our Tech Support. How can we assist you today?');
+    alert('Welcome to Apple Support. How can we assist you today?');
 });
